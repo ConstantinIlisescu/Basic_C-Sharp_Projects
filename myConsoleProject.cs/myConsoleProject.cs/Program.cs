6c1507a9e2d0f5692880dfa05798b78c65013130
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-05 15:49:13 +0100

    Console.WriteLine and .ReadLine

--- myConsoleProject.cs/myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace myConsoleProject
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
