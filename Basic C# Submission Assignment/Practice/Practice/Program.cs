8c096e99c54b9df21e962c9606c4c4f43581ddfc
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-11 19:09:16 +0100

    Create a Practice Program

---
+++ Basic C# Submission Assignment/Practice/Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Math operations
+
+
+            int num1 = 5;
+            int num2 = 7;
+
+            //Add
+            int total = num1 + num2;
+            Console.WriteLine(num1 + " + " + num2 + " = " + total);
+
+
+
+        }
+    }
+}
