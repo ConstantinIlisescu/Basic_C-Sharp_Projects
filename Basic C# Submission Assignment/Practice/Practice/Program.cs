d54ba09c986726b7c476c6c7f5f37410a33f2bb9
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-11 19:39:50 +0100

    Math with different data types

--- Basic C# Submission Assignment/Practice/Practice/Program.cs
+++ Basic C# Submission Assignment/Practice/Practice/Program.cs
@@ -35,6 +35,16 @@ namespace Practice
             //To determinate if a number is even or odd , if output is 1 than the number is odd and if the output is 0 the number is even.
             int remainder2 = num1 % 2;
 
+            //Math with different data types
+            int num4 = 7;
+            float num5 = 5.5f;
+
+            float totalFloat = num4 + num5;
+            Console.WriteLine(num4 + " + " + num5 + " = " + totalFloat);
+
+            //cast num5 - is going to round down, loosing the value after .0
+            int resultInt = num4 + (Int32)num5;
+            Console.WriteLine(num4 + " + " + num5 + " = " + resultInt);
 
 
         }
