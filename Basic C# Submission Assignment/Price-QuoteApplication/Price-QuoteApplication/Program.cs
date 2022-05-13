4aef20290f8f223217471ab70d1bea679cf11dc4
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-13 20:03:11 +0100

    Price-Quote Application Assignment Submission

--- Basic C# Submission Assignment/Price-QuoteApplication/Price-QuoteApplication/Program.cs
+++ Basic C# Submission Assignment/Price-QuoteApplication/Price-QuoteApplication/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Price_QuoteApplication
+﻿using System;
+namespace Price_QuoteApplication
 {
     internal class Program
     {
         static void Main()
         {
+            Console.WriteLine("\nWelcome to Package Express. Please follow the instructions below.\n");
+            Console.WriteLine("Please enter the package weight, in kg:");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                break;
+            }
+
         }
     }
 }
---
+++ Basic C# Submission Assignment/PriceQuoteApplication/PriceQuoteApplication/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PriceQuoteApplication
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\nWelcome to Package Express. Please follow the instructions below.\n");
+
+            Console.Write("Please enter the package weight, in kg: ");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("\nPackage to heavy to be shipped via Package Express. Have a good day.\n");
+                return;
+            }
+
+            Console.Write("\nPlease enter the package width, in cm: ");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nPlease enter the package height, in cm: ");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nPlease enter the package length, in cm: ");
+            int packageLength = Convert.ToInt32(Console.ReadLine());
+
+            if ((packageWidth + packageHeight + packageLength) > 50)
+            {
+                Console.WriteLine("\nPackage to big to be shipped via Package Express.\n");
+                return;
+            }
+
+            float shipmentCost = ((packageWidth * packageHeight * packageLength) * packageWeight) / 100;
+            Console.WriteLine("\nThe shipping cost is : £" + shipmentCost + " \n");
+
+
+        }
+    }
+}
