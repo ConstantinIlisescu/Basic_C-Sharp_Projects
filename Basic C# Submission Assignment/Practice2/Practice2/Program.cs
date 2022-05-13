0ef93df5173696e185bdcee06fb97af38aad0797
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-13 16:23:20 +0100

    Switch Statement

---
+++ Basic C# Submission Assignment/Practice2/Practice2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Switch statement
+            int number = 2;
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("The number is 1");
+                    break;
+                case 2:
+                    Console.WriteLine("The number is 2");
+                    break;
+                case 3:
+                    Console.WriteLine("The number is 3");
+                    break;
+            }
+        }
+    }
+}
