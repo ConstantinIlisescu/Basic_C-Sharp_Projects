dbbd5fdeb7d9f5d331131079bbd03d0ded75a28c
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-19 17:44:47 +0100

    Instantiate an Array without using 'new int[]'

--- Basic C# Submission Assignment/ArraysAndLists/ArraysAndLists/Program.cs
+++ Basic C# Submission Assignment/ArraysAndLists/ArraysAndLists/Program.cs
@@ -13,6 +13,8 @@ internal class Program
 
         int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
 
+        int[] numArray2 = { 5, 2, 10, 200, 5000 };
+
         Console.WriteLine(numArray1[0]);
         Console.ReadLine();
     }
