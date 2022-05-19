82bf80ccab3bd3ea9fa2e621f007f8f2068e6ee4
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-19 20:49:21 +0100

    Console App Assignment Part 4

--- Basic C# Submission Assignment/Six-Part-Assignment/Six-Part-Assignment/Program.cs
+++ Basic C# Submission Assignment/Six-Part-Assignment/Six-Part-Assignment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 //using System.Collections.Generic;
 
 
@@ -14,12 +15,12 @@ internal class Program
 
         //Ask the user to input some text.
         Console.WriteLine("Type your name.\n");
-        string userIntput = Console.ReadLine();
+        string userInput = Console.ReadLine();
 
         //A loop that iterates through each string in the array and adds the user's text input to the end of each string.
         for (int i = 0; i < elements.Length; i++)
         {
-            elements[i] = elements[i] + userIntput;
+            elements[i] = elements[i] + userInput;
             //Then create a loop that prints off each string in the array on a separate line.
             Console.WriteLine(elements[i]);
         }
@@ -58,6 +59,38 @@ internal class Program
             Console.WriteLine(i);
         }
         //Save and execute your code.
+
+        Console.WriteLine("\n\n\n");
+
+        //** Console App Assignment Part 4 **//
+        //A list of strings where each item in the list is unique.
+        List<string> listOfStrings = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "j" };
+
+        //Ask the user to input text to search for in the list.
+        Console.WriteLine("Type a letter from 'a' to 'j'.");
+        string userLetter = Console.ReadLine();
+
+        //Add code to the loop that tells a user if they put in text that isn’t in the list.
+        if (!listOfStrings.Contains(userLetter))
+        {
+            Console.WriteLine(userLetter + " is not in the list.");
+            // Add code to the loop that stops it from executing once a match has been found.
+            return;
+        }
+
+        //A loop that iterates through the list and then displays the index of the list that contains matching text on the screen.
+        for (int i = 0; i < listOfStrings.Count; i++)
+        {
+            Console.WriteLine(i);
+            if (listOfStrings[i].Equals(userLetter))
+            {
+                Console.WriteLine("Your letter is at index: " + i);
+                //Add code to the loop that stops it from executing once a match has been found.
+                return;
+            }
+
+        }
+
 
         Console.WriteLine("\n\n\n");
 
