73043931d8b44290b04f7cf0c913a4977212824e
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-19 19:16:41 +0100

    iterate over a string Array using for loop

--- Basic C# Submission Assignment/Iteration/Iteration/Program.cs
+++ Basic C# Submission Assignment/Iteration/Iteration/Program.cs
@@ -6,17 +6,32 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
 
-        for (int i = 0; i < testScores.Length; i++)
+        //******** iterate over an int Array using for loop *******//
+
+        //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+        //for (int i = 0; i < testScores.Length; i++)
+        //{
+        //    if (testScores[i] > 85)
+        //    {
+        //        Console.WriteLine("Passing test score: " + testScores[i]);
+        //    }
+        //}
+
+        //******** iterate over a string Array using for loop *******//
+
+        string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+        for (int j = 0; j < names.Length; j++)
         {
-            if (testScores[i] > 85)
+            if (names[j] == "Jesse")
             {
-                Console.WriteLine("Passing test score: " + testScores[i]);
+                Console.WriteLine(names[j]);
             }
         }
-        Console.ReadLine();
 
+        Console.ReadLine();
     }
 }
 
