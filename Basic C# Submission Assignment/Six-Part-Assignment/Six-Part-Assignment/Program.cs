04a2224df75299b6b0ce9fdb3a0d848e9e7abb1e
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-19 20:04:00 +0100

    Console App Assignment Part 1

---
+++ Basic C# Submission Assignment/Six-Part-Assignment/Six-Part-Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+//using System.Collections.Generic;
+
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        //** Console App Assignment Part 1 **//
+        //A one-dimensional array of strings.
+        string[] elements = { "Have Fun ", "Be Positive ", "Keep coding " };
+
+        //Ask the user to input some text.
+        Console.WriteLine("Type your name.\n");
+        string userIntput = Console.ReadLine();
+
+        //A loop that iterates through each string in the array and adds the user's text input to the end of each string.
+        for (int i = 0; i < elements.Length; i++)
+        {
+            elements[i] = elements[i] + userIntput;
+            //Then create a loop that prints off each string in the array on a separate line.
+            Console.WriteLine(elements[i]);
+        }
+
+
+    }
+}
+
