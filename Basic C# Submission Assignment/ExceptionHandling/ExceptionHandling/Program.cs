257bf1af6349df3fde8935cf08a34ec20613b11c
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-25 16:27:20 +0100

    ExceptionHandling Practice

---
+++ Basic C# Submission Assignment/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Pick a a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please type a whole number.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please don't devide by 0.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Catch an error.");
+            }
+            finally
+            {
+                Console.WriteLine("This is executed even if an error occures");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
