472156e14a0f193aa969f957cdc70343788b5ad7
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-26 14:54:26 +0100

    Create Card Class

---
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,13 @@
+﻿namespace TwentyOne
+{
+    public class Card
+    {
+        public Card()
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Card cardOne = new Card();
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Spades";
+
+            Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
         }
     }
 }
