330e11da01efd5ca11008d94dff1bd3df95301f0
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-26 18:01:27 +0100

    Method Assignment Submision

---
+++ Basic C# Submission Assignment/MethodAssignment/MethodAssignment/BasicMath.cs
@@ -0,0 +1,23 @@
+﻿namespace MethodAssignment
+{
+    public class BasicMath
+    {
+        public int addNumber(int userNum)
+        {
+            int result = userNum + 10;
+            return result;
+        }
+
+        public int subtractNumber(int userNum)
+        {
+            int result = userNum - 10;
+            return result;
+        }
+
+        public int multiplyNumber(int userNum)
+        {
+            int result = userNum * 10;
+            return result;
+        }
+    }
+}
---
+++ Basic C# Submission Assignment/MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a class. 
+             * In that class, create three methods, each of which will take one integer parameter in and return an integer. 
+             * The methods should do some math operation on the received parameter. 
+             * Put this class in a separate .cs file in the application.  - it's in BasicMath.cs
+             */
+            BasicMath mathOperation = new BasicMath();
+
+            //In the Main() program, ask the user what number they want to do the math operations on.
+            Console.WriteLine("Tel me a number.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+
+            //Call each method in turn, passing the user input to the method. Display the returned integer to the screen.
+            int resultAdd = mathOperation.addNumber(userInput);
+            Console.WriteLine("Your number added to 10 equals: " + resultAdd);
+
+            int resultSubtract = mathOperation.subtractNumber(userInput);
+            Console.WriteLine("Your number subtracted by 10 equals: " + resultSubtract);
+
+            int resultMultiply = mathOperation.multiplyNumber(userInput);
+            Console.WriteLine("Your number multiplied by 10 equals: " + resultMultiply);
+
+
+            Console.ReadLine();
+        }
+    }
+}
