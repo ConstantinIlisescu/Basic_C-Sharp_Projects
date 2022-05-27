5ab802e84b3ab6e5312defd9fd28031a6ca36151
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-27 12:39:35 +0100

    Classes and Methods Assignment 1

---
+++ Basic C# Submission Assignment/Classes_MethodsAsg1/Classes_MethodsAsg1/aClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Classes_MethodsAsg1
+{
+    internal class aClass
+    {
+
+        /*1.Create a class. 
+             * In that class, create a method that will take in an integer, create a math operation for this integer (addition, subtraction, etc.), 
+             * then return the answer as an integer.*/
+        public int aNumber(int userInput)
+        {
+            return userInput * 5;
+        }
+
+
+        /*3.Add a second method to the class with the same name that will take in a decimal, 
+           * create a different math operation for it, then return the answer as an integer. */
+        public decimal aNumber(decimal userinput)
+        {
+            return userinput + 5.1m;
+        }
+
+        /*5.Add a third method to the class, with the same name, that will take in a string, 
+           * convert it to an integer if possible, do a different math operation to it, then return the answer as an integer.  */
+        public int aNumber(string userInput)
+        {
+            int userInputNumber = Convert.ToInt32(userInput);
+            return userInputNumber - 10;
+        }
+
+    }
+}
