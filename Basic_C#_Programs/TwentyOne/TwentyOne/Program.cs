d5a442303dc022ab93abd9da8dad8c4f5168319c
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-28 09:57:38 +0100

    Deleted the unwanted code

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -1,27 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 namespace TwentyOne
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //instanciate the classes
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string> { "Jesse", "Bill", "Joe" };
+            Deck deck = new Deck();
+            deck.Shuffle(3);
 
-            //Calling the superclass method that is inherited from Game class
-            game.ListPlayers();
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
 
-            //Deck deck = new Deck();
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-
-            //Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine(deck.Cards.Count);
 
             Console.ReadLine();
         }
