76ed00f25c92151180551953abd2efc921a042f1
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-05-28 13:22:52 +0100

    Inheritance Assignment

---
+++ Basic C# Submission Assignment/InheritanceAssig/InheritanceAssig/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace InheritanceAssig
+{
+    //3.Create another class called Employee and have it inherit from the Person class.
+
+    internal class Employee : Person
+    {
+        //Give the Employee class a property called Id and have it be of data type int.
+        public int ID;
+    }
+}
---
+++ Basic C# Submission Assignment/InheritanceAssig/InheritanceAssig/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InheritanceAssig
+{
+    //1.Create a class called Person
+    internal class Person
+    {
+        //Give it two properties, each of data type string. One called FirstName, the other LastName.
+        public string FirstName;
+        public string LastName;
+
+        //2.Give this class a void method called SayName() that takes no parameters
+        //and simply writes the person's full name to the console in the format of: "Name: [full name]".
+        public void SayName()
+        {
+            Console.WriteLine("Name : {0} {1}.", FirstName, LastName);
+        }
+    }
+}
