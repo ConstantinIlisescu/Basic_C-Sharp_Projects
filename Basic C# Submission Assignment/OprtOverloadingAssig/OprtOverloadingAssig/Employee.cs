4eb8a23745568461f61c95ece6a895488bbdefcf
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-06-16 14:04:44 +0100

    Operator Overloading Assignment

---
+++ Basic C# Submission Assignment/OprtOverloadingAssig/OprtOverloadingAssig/Employee.cs
@@ -0,0 +1,42 @@
+﻿namespace OprtOverloadingAssig
+{
+    public class Employee
+    {
+        //Solution reference on StackOverFlow: https://stackoverflow.com/questions/25461585/operator-overloading-equals
+
+        public int ID { get; set; }
+        public string employeeName { get; set; }
+
+        public static bool operator ==(Employee A, Employee B)
+        {
+            if ((object)A == null)
+                return (object)B == null;
+
+            return A.Equals(B);
+        }
+
+        public static bool operator !=(Employee A, Employee B)
+        {
+            return !(A == B);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            var employee = (Employee)obj;
+            return (ID == employee.ID && employeeName == employee.employeeName);
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode() ^ employeeName.GetHashCode();
+        }
+
+
+
+    }
+
+
+}
---
+++ Basic C# Submission Assignment/OprtOverloadingAssig/OprtOverloadingAssig/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OprtOverloadingAssig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee1 = new Employee();
+            employee1.ID = 1;
+            employee1.employeeName = "Bubulina";
+
+            Employee employee2 = new Employee();
+            employee2.ID = 2;
+            employee2.employeeName = "Margarina";
+
+            bool result = employee1.ID == employee2.ID;
+            Console.WriteLine(result);
+
+
+            Console.ReadLine();
+        }
+    }
+}
