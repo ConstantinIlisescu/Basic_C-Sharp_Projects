1b502f278789fd9acbdd31d894ab9214ae28b59b
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-06-18 09:49:28 +0100

    Instantiate an Employee object with type "string" as its generic parameter.

--- Basic C# Submission Assignment/OprtOverloadingAssig/OprtOverloadingAssig/Program.cs
+++ Basic C# Submission Assignment/OprtOverloadingAssig/OprtOverloadingAssig/Program.cs
@@ -7,6 +7,14 @@ namespace OprtOverloadingAssig
         static void Main(string[] args)
         {
 
+
+            //3. Generics Assignment: Instantiate an Employee object with type "string" as its generic parameter. Assign a list of strings as the property value of Things.
+            Employee<string> employee = new Employee<string>();
+
+
+
+
+            //Below is part of operators overload  assignment.
             Employee<int> employee1ID = new Employee<int>();
             employee1ID.ID = 1;
 
