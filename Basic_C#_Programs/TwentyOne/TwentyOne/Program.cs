656db39340cbc1630fe3563dff46aa38f29abdef
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-06-18 09:21:19 +0100

    Update

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace TwentyOne
 {
@@ -7,13 +6,7 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            //Polymorphism, the ability of a class to morph into its inheriting class
-            Game game = new TwentyOneGame();
-            game.Players = new List<Player>();
-            Player player = new Player();
-            player.Name = "Jesse";
-            game += player; //game = game + player;
-            game -= player; //game = game - player;
+
 
             Deck deck = new Deck();
             deck.Shuffle(3);
