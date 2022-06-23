c4bc1222a84c8c7f3dbc74e51785ec9c224aa2c5
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-06-23 10:48:44 +0100

    Enums Assignment

---
+++ Basic C# Submission Assignment/EnumsAssignment/EnumsAssignment/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace EnumsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            //4.Wrap the statement in a try/catch block and have it print "Please enter an actual day of the week." to the console if an error occurs.
+            try
+            {
+
+                //2.Prompt the user to enter the current day of the week.
+                Console.WriteLine("Please enter the curent day of the week, type it or enter the number (0 being monday)");
+                string userInput = (Console.ReadLine());
+                string userInputLowerCase = userInput.ToLower();
+
+
+
+                //3.Assign the value to a variable of that enum data type you just created.
+                daysOfTheWeeks day = (daysOfTheWeeks)Enum.Parse(typeof(daysOfTheWeeks), userInputLowerCase);        // reference : https://docs.microsoft.com/en-us/dotnet/api/system.enum.parse?view=net-6.0#system-enum-parse(system-type-system-string)
+
+                Console.WriteLine("Your chosen current day is {0}", day);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+
+            Console.ReadLine();
+        }
+
+        //1.Create an enum for the days of the week.
+        public enum daysOfTheWeeks
+        {
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+            sunday
+        }
+
+    }
+}
+
+
+
+
+
+
+
