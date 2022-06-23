3fac071827e8e33a48563b18da664f5bd41a3ed0
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-06-23 09:31:11 +0100

    Modify the deck class to inplement the enum value type Face and Suit

--- Basic_C#_Programs/TwentyOne/TwentyOne/Deck.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Deck.cs
@@ -9,30 +9,15 @@ namespace TwentyOne
         //Construct method always goes above the class. This is a method that is called as soon as an object is created.
         public Deck()
         {
-
-            ////Adding one cart at a time
-            //Cards = new List<Card>();
-            //Card cardOne = new Card();
-            //cardOne.Face = "Two";
-            //cardOne.Suit = "Hearts";
-            //Cards.Add(cardOne);
-
-
-            //adding all carts using a nested for each loop
             Cards = new List<Card>();
-            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
-            List<string> Faces = new List<string>()
-            {
-                "Two", "Three", "Four", "Five", "Six" , "Seven" , "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
-            };
 
-            foreach (string face in Faces)
+            for (int i = 0; i < 13; i++)
             {
-                foreach (string suit in Suits)
+                for (int j = 0; j < 4; j++)
                 {
                     Card card = new Card();
-                    card.Suit = suit;
-                    card.Face = face;
+                    card.Face = (Face)i;
+                    card.Suit = (Suit)j;
                     Cards.Add(card);
                 }
             }
