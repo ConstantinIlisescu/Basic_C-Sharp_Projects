bf86cb31577f43fed7ff3a5121ef0f0f070c29f4
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-06-24 09:53:38 +0100

    pleyer removed when while loop ends

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -31,6 +31,9 @@ namespace TwentyOne
                     game.Play();
                 }
 
+                //When it exits the game the player gets removed
+                game -= player;
+
             }
 
         }
