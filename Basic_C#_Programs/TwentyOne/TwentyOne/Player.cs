de7c03fdddbe3cf29746753116e2549eaa0cf11a
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-06-24 09:42:05 +0100

    Create a constructor in class Player

--- Basic_C#_Programs/TwentyOne/TwentyOne/Player.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Player.cs
@@ -4,11 +4,21 @@ namespace TwentyOne
 {
     public class Player
     {
+        //constructors go at the top of the class.
+        public Player(string name, int beginningBalance)
+        {
+            Hand = new List<Card>();
+            Balance = beginningBalance;
+            Name = name;
+        }
+
+
         public List<Card> Hand { get; set; }
         public int Balance { get; set; }
         public string Name { get; set; }
         public bool isActivelyPlaying { get; set; }
 
+        //overloaded method
         public static Game operator +(Game game, Player player)
         {
             game.Players.Add(player);
