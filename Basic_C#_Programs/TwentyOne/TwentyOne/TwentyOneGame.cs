faf510ab4dfdb3a97789860beea54e3d68f2d30f
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-06-24 10:41:31 +0100

    Add Dealer property, and instantiate a dealer

--- Basic_C#_Programs/TwentyOne/TwentyOne/TwentyOneGame.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -5,9 +5,19 @@ namespace TwentyOne
     //A clas can only have one base class inherited, but it can inherit as many interfaces as it needs.
     public class TwentyOneGame : Game, IWalkAway
     {
+        public TwentyOneDealer Dealer { get; set; }
+
+        //this is an override method because is an abstract method in Game(we have to ahve an implementation of it)
         public override void Play()
         {
-            throw new NotImplementedException();
+            //instantiate a dealer
+            Dealer = new TwentyOneDealer();
+
+            foreach (Player player in Players)
+            {
+
+            }
+
         }
 
         public override void ListPlayers()
