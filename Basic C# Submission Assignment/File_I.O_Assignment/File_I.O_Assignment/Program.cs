fe91fcb2b0963430399bb02c4d3fa857cdfe4e8d
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-02 17:09:41 +0100

    Log that number to a text file.

--- Basic C# Submission Assignment/File_I.O_Assignment/File_I.O_Assignment/Program.cs
+++ Basic C# Submission Assignment/File_I.O_Assignment/File_I.O_Assignment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace File_I.O_Assignment
 {
@@ -8,6 +9,15 @@ namespace File_I.O_Assignment
         {
             //1. Asks the user for a number.
             Console.WriteLine("Type a number.");
+
+            //2. Logs that number to a text file.
+            string userInput1 = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\coste\Desktop\userInputLogFile.txt", userInput1);
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
