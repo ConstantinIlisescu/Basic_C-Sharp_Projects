254ba52d746b895bc114b3bb4c9f7f08f80c40d8
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-02 17:58:13 +0100

    Print the current date and time to the console.

---
+++ Basic C# Submission Assignment/DatetimeAssignment/DatetimeAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DatetimeAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Prints the current date and time to the console.
+            Console.WriteLine(DateTime.Now);
+
+            Console.ReadLine();
+        }
+    }
+}
