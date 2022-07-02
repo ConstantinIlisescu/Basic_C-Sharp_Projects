499ab780d7f4d4d61fea789dbfa7487d6bf99945
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-02 18:00:58 +0100

    Ask the user for a number.

--- Basic C# Submission Assignment/DatetimeAssignment/DatetimeAssignment/Program.cs
+++ Basic C# Submission Assignment/DatetimeAssignment/DatetimeAssignment/Program.cs
@@ -9,6 +9,11 @@ namespace DatetimeAssignment
             //1. Prints the current date and time to the console.
             Console.WriteLine(DateTime.Now);
 
+            //2. Asks the user for a number.
+            Console.WriteLine("Type a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+
             Console.ReadLine();
         }
     }
