85f05de1cc073ee5cdc7b3d855109e5a6bc59b39
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-02 17:02:26 +0100

    Ask the user for a number.

---
+++ Basic C# Submission Assignment/File_I.O_Assignment/File_I.O_Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace File_I.O_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Asks the user for a number.
+            Console.WriteLine("Type a number.");
+        }
+    }
+}
