68e952fdef9ad1c40b032729d945448dfb60e4f3
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-02 18:19:08 +0100

    Print to the console the exact time it will be in X hours, X being the number the user entered in step 2.

--- Basic C# Submission Assignment/DatetimeAssignment/DatetimeAssignment/Program.cs
+++ Basic C# Submission Assignment/DatetimeAssignment/DatetimeAssignment/Program.cs
@@ -7,12 +7,17 @@ namespace DatetimeAssignment
         static void Main(string[] args)
         {
             //1. Prints the current date and time to the console.
-            Console.WriteLine(DateTime.Now);
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine(currentDate);
 
             //2. Asks the user for a number.
             Console.WriteLine("Type a number");
             int userInput = Convert.ToInt32(Console.ReadLine());
 
+            //3. Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+
+
+            Console.WriteLine("The current time {0} + {1} hours equals {2}", currentDate, userInput, currentDate.AddHours(userInput));
 
             Console.ReadLine();
         }
