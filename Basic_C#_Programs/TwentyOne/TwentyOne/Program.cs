aca2d7d87c126e649481b6fd99b015641b5d2a2f
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-05 12:04:27 +0100

    Add a const string variable - const can't change their value once they were defined.

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -8,8 +8,8 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            const string casinoName = "Grand Hotel and Casino";
+            Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
 
             Console.WriteLine("How much money did you bring today?");
