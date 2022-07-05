160de8886d402b21393856f136189454183299e5
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-05 12:56:46 +0100

    Create a variable using the keyword "var.”

--- Basic C# Submission Assignment/AdditionalFeaturesAssig/AdditionalFeaturesAssig/Program.cs
+++ Basic C# Submission Assignment/AdditionalFeaturesAssig/AdditionalFeaturesAssig/Program.cs
@@ -7,6 +7,9 @@
             //1. Create a const variable.
             const string aVariable = "This is a const variable!";
 
+            //2. Create a variable using the keyword "var.”
+            var aImplicitlyTypedVariable = "And this is a var variable which the compiler determined the type.";
+
 
         }
     }
