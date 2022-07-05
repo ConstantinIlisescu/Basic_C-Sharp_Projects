6d0649c37ff4cb80727c1ccadd41fcfc874cc634
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-05 12:50:37 +0100

    Create a const variable.

---
+++ Basic C# Submission Assignment/AdditionalFeaturesAssig/AdditionalFeaturesAssig/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AdditionalFeaturesAssig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Create a const variable.
+            const string aVariable = "This is a const variable!";
+
+
+        }
+    }
+}
