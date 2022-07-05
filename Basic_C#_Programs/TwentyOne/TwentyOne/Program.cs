f579b293a9762b8073966d74cb605b9470f6875c
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-05 12:32:56 +0100

    Add a Guid - Global unique identifier

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -9,6 +9,9 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             const string casinoName = "Grand Hotel and Casino";
+
+            Guid identifier = Guid.NewGuid();
+
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
 
