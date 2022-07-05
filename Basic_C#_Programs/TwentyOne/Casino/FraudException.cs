7c248edba298b03d661744367e99e72a407a5346
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-05 14:24:46 +0100

    Create my own exception that inherits from the base Exception class

---
+++ Basic_C#_Programs/TwentyOne/Casino/FraudException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Casino
+{
+    internal class FraudException : Exception // inherits from Exception
+    {
+        public FraudException()
+            : base() { }
+        public FraudException(string message)
+            : base(message) { }
+    }
+}
