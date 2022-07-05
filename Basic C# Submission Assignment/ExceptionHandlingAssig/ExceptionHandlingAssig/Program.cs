44f97123e15611608143369a2ef3bd9cf83d4fa4
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-05 14:33:18 +0100

    Create  ExceptionHandlingAssig project

---
+++ Basic C# Submission Assignment/ExceptionHandlingAssig/ExceptionHandlingAssig/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ExceptionHandlingAssig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1. Ask the user for their age.
+
+        }
+    }
+}
