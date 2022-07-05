e2ad60cd38586c59694337e16e1233cf9764fe98
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-05 11:43:47 +0100

    Remove the Calss files from the twenty one project to the Class library Casino.

--- Basic_C#_Programs/TwentyOne/TwentyOne/IWalkAway.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TwentyOne
-{
-    //Interfaces are always public, also their methodes.
-    //A clas can inherite as many interfaces as it needs.
-    interface IWalkAway
-    {
-        void WalkAway(Player player);
-    }
-}
